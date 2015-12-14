27d20b2fe5e6e58be037bbbbf64e8de7f4efd053
Razvan Preda <rcpreda@gmail.com>
2015-12-14 15:03:43 +0000

    Permissions controller and views has been added

---
+++ resources/views/permissions/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+
+
+    <div class="row">
+
+        <div class="col-md-6 col-md-offset-3">
+            <h3>Permission controller Edit</h3>
+
+        </div>
+
+    </div>
+@stop
---
+++ resources/views/permissions/store.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('content')
+
+
+    <div class="row">
+
+        <div class="col-md-6 col-md-offset-3">
+            <h3>Permission controller Store</h3>
+            <a href="">Create</a>
+            <a href="{{ route('admin.permissions.edit', 1) }}">Edit</a>
+            <a href="{{ route('admin.permissions.destroy', 1) }}">Delete</a>
+            <a href="{{ route('admin.permissions.show', 1) }}">Show</a>
+        </div>
+
+    </div>
+@stop
