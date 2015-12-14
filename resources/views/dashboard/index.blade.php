ac1d8aed2b5fb1917885b641f663f58cebeb29ad
Razvan Preda <rcpreda@gmail.com>
2015-12-14 15:04:48 +0000

    Dashboad index view added

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,16 @@
-<h3>This is dashboard index</h3>
+@extends('layout')
+
+@section('content')
+
+
+    <div class="row">
+
+        <div class="col-md-6 col-md-offset-3">
+            <h3>This is dashboard index</h3>
+
+        </div>
+
+    </div>
+@stop
+
+
